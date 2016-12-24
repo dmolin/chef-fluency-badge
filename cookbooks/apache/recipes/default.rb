400d14530fcd620f245e4da634cce535e0c47e60
davide <davide@localhost.localdomain>
2016-12-24 13:51:44 +0000

    Add motd and using node attributes

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,8 +4,15 @@
 #
 # Copyright:: 2016, Davide Molin, All Rights Reserved.
 
+# let's query the local node to see which OS we're installing onto
+if node['platform_family'] == 'rhel'
+	package = 'httpd'
+else
+	package = 'apache2'
+end
+
 package 'apache2' do
-	package_name 'httpd'
+	package_name package
 	action :install
 end
 
